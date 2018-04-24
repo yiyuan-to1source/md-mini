0696bcfcb4d6347671defe2c16a0053deba0feab
to1source <to1source@foxmail.com>
2018-04-24 12:52:38 +0800

    setting up all the devs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const config = require('config');
@@ -9,12 +10,12 @@ const { join } = require('path');
 
 const paths = config.get('paths');
 
-exports.sassTask = build => {
+const sassTask = build => {
   return () => {
     const dest = build ? paths.dest : paths.dev;
-    const dir = join(__dirname, '..' ,dest, paths.style);
+    const dir = join(__dirname, '..' ,dest);
     // console.log('style dir', dir);
-    return gulp.src( join(__dirname, '..', paths.app, paths.style, 'main.scss') )
+    return gulp.src(join(__dirname, '..', paths.src, 'md-mini.scss'))
       .pipe(sourcemaps.init())
       .pipe( sass({
         includePaths: join(process.cwd(), 'node_modules')
@@ -23,6 +24,7 @@ exports.sassTask = build => {
         autoprefixer,
         cssnano
       ]))
+      .pipe(rename('md-mini.min.css'))
       .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest(dir));
   };
