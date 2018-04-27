8af5024ca976448e5f0cf17bc0c53a2e6d0bacc9
to1source <to1source@foxmail.com>
2018-04-27 14:06:04 +0800

    create the less framework structure

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 const server = require('gulp-server-io');
+const less = require('gulp-less');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const config = require('config');
@@ -30,6 +31,13 @@ const sassFn = dest => () => {
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(dest));
 };
+
+const lessFn = dest => () => {
+  return gulp.src(join(__dirname, paths.src, 'md-mini.less'))
+    .pipe(sourcemaps.init())
+    .pipe(less())
+}
+
 // dev sass task
 gulp.task('sass:dev', sassFn(join(__dirname, paths.dev)));
 gulp.task('sass:build', sassFn(join(__dirname, paths.dest)));
